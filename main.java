b1375daaad7fe8a65ebd38302696b6494e0526a3
ChrisYMG <cmejiagalindo@yahoo.com>
2024-05-12 00:49:14 -0600

    Revert "JDBC, git ignore"
    This reverts commit b3ed4115cdf6ff6b79677924ef80c279266a16fa.

--- main.java
+++ main.java
@@ -9,9 +9,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Properties;
 
 // Modelo
 class ScholarSearch {
@@ -43,13 +40,7 @@ class ConsoleView {
 // Controlador
 public class main {
     public static void main(String[] args) throws Exception {
-        Properties prop = new Properties();
-        InputStream input = new FileInputStream("config.properties");
-        prop.load(input);
-
-        String api_key = prop.getProperty("API_KEY");
-
-        String apiKey = api_key;
+        String apiKey = "api_key";
         ScholarSearch model = new ScholarSearch(apiKey);
         ConsoleView view = new ConsoleView();
 
